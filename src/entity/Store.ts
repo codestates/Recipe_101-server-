a8499c4ea281ab11db77dbc39c1fffda3816e3bb
karuiner <intaekgong@gmail.com>
2021-05-18 16:39:33 +0900

    [server] feature/ make entities

---
+++ src/entity/Store.ts
@@ -0,0 +1,13 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity()
+export class Store {
+  @PrimaryGeneratedColumn()
+  id: number;
+}
--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,18 +1,33 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
 
 @Entity()
 export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @PrimaryGeneratedColumn()
-    id: number;
+  @Column()
+  userName: string;
 
-    @Column()
-    firstName: string;
+  @Column()
+  password: string;
 
-    @Column()
-    lastName: string;
+  @Column()
+  password2: string;
 
-    @Column()
-    age: number;
+  @Column()
+  email: string;
 
+  @Column()
+  phone: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+  @UpdateDateColumn()
+  updated_at: Date;
 }
