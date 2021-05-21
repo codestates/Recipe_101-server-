01816917c0397d83f0a7a9dda0fa6b4203a5848e
karuiner <intaekgong@gmail.com>
2021-05-22 05:53:53 +0900

    [server] fix/ 주석처리 제거

--- src/controller/search/itemname.ts
+++ src/controller/search/itemname.ts
@@ -5,26 +5,6 @@ import { FoodInfo } from "../../entity/FoodInfo";
 const itemname = express.Router();
 
 itemname.get("/:itemname", (req, res) => {
-  // let q = getRepository(FoodInfo)
-  //   .createQueryBuilder("f")
-  //   .innerJoinAndSelect(Ingredients, "i")
-  //   .where("f.id = i.foodInfoId ")
-  //   .andWhere(`i.name = :name `, { name: req.params.itemname })
-  //   .select([
-  //     "f.id AS food_id",
-  //     "f.foodName AS food_name",
-  //     "f.img_url AS food_img",
-  //     "f.level AS level",
-  //     "f.cooking_time AS cooking_time",
-  //   ])
-  //   .orderBy("f.foodName")
-  //   .execute()
-  //   .then((rst) => {
-  //     res.status(200).json({ data: { recipe: rst }, message: "ok" });
-  //   })
-  //   .catch((err) => {
-  //     res.status(400).send("fail");
-  //   });
   let items = req.params.itemname.split("&");
   items = items.map((x: string, i: number) => {
     let ans = {},
