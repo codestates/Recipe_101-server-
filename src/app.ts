c45723f9467779c33a21b48842f8a0b368b2e301
karuiner <intaekgong@gmail.com>
2021-05-23 17:00:35 +0900

    [server] fix/ 연결 클라이언트 호스트 추가

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,11 @@ createConnection()
     app.use(cookieParser());
     app.use(
       cors({
-        origin: ["http://localhost:3000", process.env.CLIENT_HOST],
+        origin: [
+          "http://localhost:3000",
+          process.env.CLIENT_HOST,
+          process.env.CLIENT_HOST2,
+        ],
         credentials: true,
         exposedHeaders: "*",
       })
